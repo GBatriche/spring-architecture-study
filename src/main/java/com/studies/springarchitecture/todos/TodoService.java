38c0fefdf3196f1a775dada39b607d7e1a5b9ddd
GBatriche <batricheg@gmail.com>
2025-09-09 10:42:13 -0500

    the entire flowchart was created

---
+++ src/main/java/com/studies/springarchitecture/todos/TodoService.java
@@ -0,0 +1,18 @@
+package com.studies.springarchitecture.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity save(TodoEntity newTodo){
+        return repository.save(newTodo);
+    }
+
+}
